03949770c9b251a1fac09a928bd2f6e594a3a891
Jacek Kowalczyk <zxspace1@gmail.com>
2018-10-13 16:52:03 +0200

    Part 1

--- slider/js/main.js
+++ slider/js/main.js
@@ -1,5 +1,24 @@
-console.log($('p'));
+const slideShow = $('.slide-show');
+const slides = $('.single-slide');
+const prev = $('slide-nav-prev');
+const next = $('slide-nav-next');
+let slideCount = slides.length;
+let slideWidth = 100/slideCount;
+let slideIndex = 0;
 
-$(function() {
-  console.log('AAAA');
-  })
+slideShow.css({'width': slideCount + '00%'});
+
+slides.each(function(ind){
+    $(this).css({
+        'width': slideWidth + '%',
+        'margin-left': ind * slideWidth + '%',
+    });
+});
+
+prev.on('click',function(){
+    
+})
+
+next.on('click',function(){
+    
+})
