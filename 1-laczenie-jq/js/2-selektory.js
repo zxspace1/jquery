ac9da181297dde2c8a032563efbda42290471c76
Jacek Kowalczyk <zxspace1@gmail.com>
2018-10-13 11:51:40 +0200

    selekt

---
+++ 1-laczenie-jq/js/2-selektory.js
@@ -0,0 +1,16 @@
+// pobieranie po tagu html
+
+var pAll = $('p')
+//console.log(pAll);
+
+// pobieranie po klasie
+
+
+var pClass = $('.paragraf');
+//console.log(pClass);
+
+// po atrybucie
+
+let link = $('[href^="mailto:"]');
+
+//console.log(link);
