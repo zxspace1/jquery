e8238b0e07b54d3c1fd4628e49da13eb68cd465c
Jacek Kowalczyk <zxspace1@gmail.com>
2018-10-14 10:10:36 +0200

    slider

--- slider/js/main.js
+++ slider/js/main.js
@@ -1,24 +1,86 @@
+//const slideShow = $('.slide-show');
+//const slides = $('.single-slide');
+//const prev = $('.slide-nav-prev');
+//const next = $('.slide-nav-next');
+//let slideCount = slides.length;
+//let slideWidth = 100 / slideCount;
+//let slideIndex = 0;
+//
+//function slide(newSlideIndex) {
+//    if (newSlideIndex < 0 || newSlideIndex > slideCount - 1) {
+//        return;
+//    }
+//    let marginLeft = (newSlideIndex * (-100)) + '%';
+//
+//    slideShow.animate({
+//        'margin-left': marginLeft
+//    }, 1000);
+//
+//    slideIndex = newSlideIndex;
+//}
+//
+//
+//slideShow.css({
+//    'width': slideCount + '00%'
+//});
+//
+//slides.each(function (ind) {
+//    $(this).css({
+//        'width': slideWidth + '%',
+//        'margin-left': ind * slideWidth + '%'
+//    });
+//});
+//
+//prev.on('click', function () {
+//    slide(slideIndex - 1);
+//});
+//
+//next.on('click', function () {
+//    slide(slideIndex + 1);
+//});
 const slideShow = $('.slide-show');
 const slides = $('.single-slide');
-const prev = $('slide-nav-prev');
-const next = $('slide-nav-next');
+const prev = $('.slider-nav-prev');
+const next = $('.slider-nav-next');
 let slideCount = slides.length;
-let slideWidth = 100/slideCount;
+let slideWidth = 100 / slideCount;
 let slideIndex = 0;
 
-slideShow.css({'width': slideCount + '00%'});
 
-slides.each(function(ind){
+function slide(newSlideIndex) {
+
+    if (newSlideIndex < 0 || newSlideIndex > slideCount - 1) {
+        return;
+    }
+
+
+
+    let marginLeft = (newSlideIndex * (-100)) + '%';
+
+
+    slideShow.animate({
+        'margin-left': marginLeft
+    }, 1000);
+    slideIndex = newSlideIndex;
+}
+
+
+
+slideShow.css({
+    'width': slideCount + '00%'
+});
+
+slides.each(function (ind) {
     $(this).css({
         'width': slideWidth + '%',
-        'margin-left': ind * slideWidth + '%',
+        'margin-left': ind * slideWidth + '%'
     });
 });
 
-prev.on('click',function(){
-    
-})
+prev.on('click', function () {
+    slide(slideIndex - 1);
+});
 
-next.on('click',function(){
-    
-})
+next.on('click', function () {
+    slide(slideIndex + 1);
+});
